2fc880be4605a44dedac49ec4aaf53073cc9005b
Jonny Smith <jonny.asmith@gmail.com>
2015-10-17 15:46:06 +0100

    added last config files and fixed vim alias

--- install/link.sh
+++ install/link.sh
@@ -1,1 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+DOTFILES=$HOME/.dotfiles
+
+echo "creating symlinks"
+linkables=$( find -H "$DOTFILES" -maxdepth 3 -name '*.symlink' )
+for file in $linkables ; do
+    target="$HOME/.$( basename $file ".symlink" )"
+    echo "creating symlink for $file"
+    ln -s $DOTFILES/$file $target
+done
