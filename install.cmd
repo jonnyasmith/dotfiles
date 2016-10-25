a8196a657eaab350e44899b0593af5aa1424086e
Jonny Smith <jonny.asmith@gmail.com>
2016-10-25 10:07:36 +0100

    added rider config

--- install.cmd
+++ install.cmd
@@ -3,5 +3,7 @@ mklink C:\Users\JonnySmith\.bashrc C:\Users\JonnySmith\.dotfiles\bash_current\.b
 mklink C:\Users\JonnySmith\.gitconfig C:\Users\JonnySmith\.dotfiles\bash_current\gitconfig.bash
 mklink C:\Users\JonnySmith\.vimrc C:\Users\JonnySmith\.dotfiles\vim\.vimrc
 mklink /D C:\Users\JonnySmith\.vim C:\Users\JonnySmith\.dotfiles\vim\.vim
+mklink /D C:\Users\JonnySmith\.webstorm C:\Users\JonnySmith\.dotfiles\.webstorm
+mklink /D C:\Users\JonnySmith\.rider C:\Users\JonnySmith\.dotfiles\.rider
 
 mkdir C:\Users\JonnySmith\.vim-tmp
