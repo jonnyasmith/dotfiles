ee52665c3646daf704bc90b768eb3521ceaaa65e
Jonny Smith <jonny.asmith@gmail.com>
2016-12-30 07:36:05 +0000

    updated more stuff

--- install.cmd
+++ install.cmd
@@ -1,27 +1,24 @@
-del C:\Users\JonnySmith\.bash_profile
-del C:\Users\JonnySmith\.bashrc
-del C:\Users\JonnySmith\.gitconfig
-del C:\Users\JonnySmith\.vimrc
-del C:\Users\JonnySmith\AppData\Roaming\Code\User\keybindings.json
-del C:\Users\JonnySmith\AppData\Roaming\Code\User\settings.json
+del /f C:\Users\Jonny\.bash_profile
+del /f C:\Users\Jonny\.bashrc
+del /f C:\Users\Jonny\.gitconfig
+del /f C:\Users\Jonny\.vimrc
+RD /S /Q C:\Users\Jonny\.vim
+RD /S /Q C:\Users\Jonny\.webstorm
+RD /S /Q C:\Users\Jonny\.rider
+del /f C:\Users\Jonny\AppData\Roaming\Code\User\settings.json
+del /f C:\Users\Jonny\AppData\Roaming\Code\User\keybindings.json
+RD /S /Q C:\Users\Jonny\.vim-tmp
 
-rmdir C:\Users\JonnySmith\.vim
-rmdir C:\Users\JonnySmith\.webstorm
-rmdir C:\Users\JonnySmith\.rider
+mklink C:\Users\Jonny\.bash_profile C:\Users\Jonny\.dotfiles\bash_current\bash_profile.bash
+mklink C:\Users\Jonny\.bashrc C:\Users\Jonny\.dotfiles\bash_current\.bashrc
+mklink C:\Users\Jonny\.gitconfig C:\Users\Jonny\.dotfiles\bash_current\gitconfig.bash
+mklink C:\Users\Jonny\.vimrc C:\Users\Jonny\.dotfiles\vim\.vimrc
+mklink /D C:\Users\Jonny\.vim C:\Users\Jonny\.dotfiles\vim\.vim
+mklink /D C:\Users\Jonny\.webstorm C:\Users\Jonny\.dotfiles\.webstorm
+mklink /D C:\Users\Jonny\.rider C:\Users\Jonny\.dotfiles\.rider
+mklink C:\Users\Jonny\AppData\Roaming\Code\User\settings.json C:\Users\Jonny\.dotfiles\.code\settings.json
+mklink C:\Users\Jonny\AppData\Roaming\Code\User\keybindings.json C:\Users\Jonny\.dotfiles\.code\keybindings.json
 
-rmdir C:\Users\JonnySmith\.vim-tmp
-
-mklink C:\Users\JonnySmith\.bash_profile C:\Users\JonnySmith\.dotfiles\bash_current\bash_profile.bash
-mklink C:\Users\JonnySmith\.bashrc C:\Users\JonnySmith\.dotfiles\bash_current\.bashrc
-mklink C:\Users\JonnySmith\.gitconfig C:\Users\JonnySmith\.dotfiles\bash_current\gitconfig.bash
-mklink C:\Users\JonnySmith\.vimrc C:\Users\JonnySmith\.dotfiles\vim\.vimrc
-mklink C:\Users\JonnySmith\AppData\Roaming\Code\User\keybindings.json C:\Users\JonnySmith\.dotfiles\.code\keybindings.json
-mklink C:\Users\JonnySmith\AppData\Roaming\Code\User\settings.json C:\Users\JonnySmith\.dotfiles\.code\settings.json
-
-mklink /D C:\Users\JonnySmith\.vim C:\Users\JonnySmith\.dotfiles\vim\.vim
-mklink /D C:\Users\JonnySmith\.webstorm C:\Users\JonnySmith\.dotfiles\.webstorm
-mklink /D C:\Users\JonnySmith\.rider C:\Users\JonnySmith\.dotfiles\.rider
-
-mkdir C:\Users\JonnySmith\.vim-tmp
+mkdir C:\Users\Jonny\.vim-tmp
 
 pause
