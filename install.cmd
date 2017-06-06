66cb77685fe874310083ec828a9d0e0a7c9147d0
Jonny Smith <jonny.asmith@gmail.com>
2017-06-06 09:16:27 +0100

    Fixed install.cmd script path for bashrc

--- install.cmd
+++ install.cmd
@@ -6,7 +6,7 @@ RD /S /Q %UserProfile%\.vim
 RD /S /Q %UserProfile%\.vim-tmp
 
 mklink %UserProfile%\.bash_profile %UserProfile%\.dotfiles\bash_current\bash_profile.bash
-mklink %UserProfile%\.bashrc %UserProfile%\.dotfiles\bash_current\.bashrc
+mklink %UserProfile%\.bashrc %UserProfile%\.dotfiles\bash_current\bashrc.bash
 mklink C:\Users\smithjon\.gitconfig C:\Users\smithjon\.dotfiles\bash_current\gitconfig.bash
 mklink %UserProfile%\.vimrc %UserProfile%\.dotfiles\vim\.vimrc
 mklink /D %UserProfile%\.vim %UserProfile%\.dotfiles\vim\.vim
