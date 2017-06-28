9000023dc78ef42b0c7eacb4a0f10fe52f30f5da
Jonny Smith <jonny.asmith@gmail.com>
2017-06-28 12:38:48 +0100

    Updates to gitconfig and install.cmd

--- install.cmd
+++ install.cmd
@@ -8,8 +8,8 @@ RD /S /Q %UserProfile%\.vim-tmp
 mklink %UserProfile%\.bash_profile %UserProfile%\.dotfiles\bash_current\bash_profile.bash
 mklink %UserProfile%\.bashrc %UserProfile%\.dotfiles\bash_current\bashrc.bash
 mklink C:\Users\smithjon\.gitconfig C:\Users\smithjon\.dotfiles\bash_current\gitconfig.bash
-mklink %UserProfile%\.vimrc %UserProfile%\.dotfiles\vim\.vimrc
-mklink /D %UserProfile%\.vim %UserProfile%\.dotfiles\vim\.vim
+mklink %UserProfile%\.vimrc %UserProfile%\.dotfiles\vim\vimrc.symlink
+mklink /D %UserProfile%\.vim %UserProfile%\.dotfiles\vim\vim.symlink
 
 mkdir %UserProfile%\.vim-tmp
 
