08c78e1707e07eddc6c3d233e70f9f48b37347bc
Jonny Smith <jonny.asmith@gmail.com>
2020-07-09 08:28:53 +0100

    changes

--- install/brew.sh
+++ install/brew.sh
@@ -1,44 +1,27 @@
 #!/bin/sh
 
-# development tools
-echo ""
-echo "Installing git"
-brew install git
-echo ""
-echo "Install tmux"
-brew install tmux
-echo ""
-echo "Installing iterm2"
-brew cask install iterm2
-echo ""
-echo "Installing zsh"
-brew install zsh
-echo ""
-echo "Installing Alfred"
-brew cask install alfred
-echo ""
-echo "Installing Google Chrome"
-brew cask install google-chrome
-echo ""
-echo "Installing Visual Studio Code"
-brew cask install visual-studio-code
-echo ""
-echo "Installing 1Password"
-brew cask install 1password
-echo ""
-echo "Installing Nord VPN"
-brew cask install nordvpn
-echo ""
-echo "Installing Spotify"
-brew cask install spotify
-echo ""
-echo "Installing slack"
-brew cask install slack
-echo ""
-echo "Installing whatsapp"
-brew cask install whatsapp
-echo ""
-echo "Installing oh-my-zsh"
-$ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo ""
-echo "Installation complete..."
+taps=(
+    git
+    tmux
+    zsh
+)
+
+brew install ${taps[@]}
+
+apps=(
+    1password
+    alfred
+    google-chrome
+    iterm2
+    microsoft-office
+    microsoft-teams
+    nordvpn
+    parallels
+    slack
+    spectacle
+    spotify
+    visual-studio-code
+    whatsapp
+)
+
+brew cask install --appdir="/Applications" ${apps[@]}
