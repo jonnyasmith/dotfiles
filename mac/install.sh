e977d564d2195a46c64afe650f1ee63e8a7da4cf
Jonny Smith <jonny.asmith@gmail.com>
2023-12-02 12:18:58 +0000

    remove dotfiles

--- mac/install.sh
+++ mac/install.sh
@@ -3,9 +3,6 @@
 echo "Configure Mac..."
 source install/macosx.sh
 
-echo "Installing dotfiles"
-source install/dotfiles.sh
-
 echo ""
 echo "Installing homebrew packages..."
 source install/brew.sh
