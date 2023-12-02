5b46e526b8eea518166ba73e7935f3d576891c35
Jonny Smith <jonny.asmith@gmail.com>
2023-12-02 12:46:08 +0000

    install oh-my-zsh

--- mac/install.sh
+++ mac/install.sh
@@ -8,6 +8,10 @@ echo "Installing homebrew packages..."
 source install/brew.sh
 
 echo ""
+echo "Install OH-MY-ZSH..."
+source install/zsh.sh
+
+echo ""
 echo "Symlinking dotfiles..."
 source install/link.sh
 
