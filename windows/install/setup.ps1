ac0bcc5ea1a8f74ec05b49a47fd6f1c2248fe590
Jonny Smith <jonny.asmith@gmail.com>
2023-12-23 15:29:08 +0000

    update windows install

--- windows/install/setup.ps1
+++ windows/install/setup.ps1
@@ -3,5 +3,5 @@ Write-Host "Running setup script" -ForegroundColor Green
 Write-Host "Install packer" -ForegroundColor Blue
 git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
 
-Write-Host "Install wsl" -ForegroundColor Blue
-wsl --install
+# Write-Host "Install wsl" -ForegroundColor Blue
+# wsl --install
