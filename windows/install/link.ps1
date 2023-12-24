00b5ac0e9d4d1bc9c44239c2bf68e2748658171b
Jonny Smith <jonny.smith@digitaljs.com>
2023-12-24 09:46:40 +0000

    fix ideavimrc

--- windows/install/link.ps1
+++ windows/install/link.ps1
@@ -1,6 +1,7 @@
 Write-Host "Running link script" -ForegroundColor Green
-Write-Host "Creating symlinks" -ForegroundColor Blue
+Write-Host "Creating symlinks"   -ForegroundColor Blue
 
-New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.config" -Target "$env:USERPROFILE\.dotfiles\config" -Force
-New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\local\nvim" -Target "$env:USERPROFILE\.dotfiles\config\nvim" -Force
-New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.gitconfig" -Target "$env:USERPROFILE\.dotfiles\windows\git\gitconfig.symlink" -Force
+New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.config"            -Target "$env:USERPROFILE\.dotfiles\config"                        -Force
+New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\AppData\local\nvim" -Target "$env:USERPROFILE\.dotfiles\config\nvim"                   -Force
+New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.gitconfig"         -Target "$env:USERPROFILE\.dotfiles\windows\git\gitconfig.symlink" -Force
+New-Item -ItemType SymbolicLink -Path "$env:USERPROFILE\.ideavimrc"         -Target "$env:USERPROFILE\.dotfiles\windows\vim\ideavimrc.symlink" -Force
