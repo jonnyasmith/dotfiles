d7c7ef51e7167b5271f0f091e1612ead8e243a81
Jonny Smith <jonny.smith@digitaljs.com>
2023-12-24 15:35:05 +0000

    update readme and add new winget packages

--- windows/install/setup.ps1
+++ windows/install/setup.ps1
@@ -1,7 +1,13 @@
 Write-Host "Running setup script" -ForegroundColor Green
 
 Write-Host "Install packer" -ForegroundColor Blue
-git clone https://github.com/wbthomason/packer.nvim "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+$packerPath = "$env:LOCALAPPDATA\nvim-data\site\pack\packer\start\packer.nvim"
+if (Test-Path $packerPath) {
+  Write-Host "packer.nvim repository already cloned"
+} else {
+  Write-Host "Cloning packer.nvim repository"
+  git clone https://github.com/wbthomason/packer.nvim $packerPath
+}
 
-# Write-Host "Install wsl" -ForegroundColor Blue
-# wsl --install
+Write-Host "Copy fzf.exe" -ForegroundColor Blue
+Copy-Item -Path "$env:USERPROFILE\.dotfiles\windows\files\fzf.exe" -Destination "C:\Windows\System32" -Force
