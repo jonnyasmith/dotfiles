dc0a7e518800f397cdf5824c1127c6a14139d276
Jonny Smith <jonny.smith@digitaljs.com>
2023-12-30 13:41:23 +0000

    update packages

--- linux/install/linux.sh
+++ linux/install/linux.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+# Check if apt is installed
+if ! command -v app &> /dev/null
+then
+    printf "apt could not be found\n"
+    exit
+fi
+
+# Update the package list and upgrade installed packages
+echo "Updating packages..."
+apt update
+apt upgrade -y
