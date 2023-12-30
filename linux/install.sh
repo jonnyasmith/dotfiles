05b6bfb24489d4e638dd821cdc8b9ede32f8bbac
Jonny Smith <jonny.smith@digitaljs.com>
2023-12-30 13:22:26 +0000

    add linux installer

---
+++ linux/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Configure Linux..."
+
+echo ""
+echo "Symlinking dotfiles..."
+
+echo ""
+echo "Installing packages..."
+source install/brew.sh
+
+echo ""
+echo "Install OH-MY-ZSH..."
+
+echo ""
+echo "Done."
