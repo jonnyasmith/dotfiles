3c946757d061584b107050c99158f1491924aa5e
Jonny Smith <jonny.asmith@gmail.com>
2023-12-30 14:42:20 +0000

    ln nvim

--- linux/install/links.sh
+++ linux/install/links.sh
@@ -2,3 +2,6 @@
 
 echo "Symlinking starship.toml..."
 ln -sf /home/jonny/.dotfiles/config/starship.toml /home/jonny/.config/starship.toml
+
+echo "Symlinking nvim..."
+ln -sf /home/jonny/.dotfiles/config/nvim /home/jonny/.config/nvim
