b28063c672999b6b83ccb7fb8643d2cffd69ca60
Jonny Smith <jonny.asmith@gmail.com>
2024-02-16 16:28:29 +0000

    add fedora setup

--- config/.zshrc
+++ config/.zshrc
@@ -153,4 +153,8 @@ fd() {
   cd "$dir"
 }
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 eval "$(starship init zsh)"
---
+++ fedora/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Configure Linux..."
+source install/linux.sh
+
+echo ""
+echo "Symlinking dotfiles..."
+source install/links.sh
+
+echo ""
+echo "Installing packages..."
+source install/packages.sh
+
+echo ""
+echo "Install OH-MY-ZSH..."
+
+echo ""
+echo "Done."
