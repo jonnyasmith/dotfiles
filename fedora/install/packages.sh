ea1cd80a61e5262b3b21d5fcdf2ebda7d4580570
Jonny Smith <jonny.asmith@gmail.com>
2024-02-16 16:29:36 +0000

    sort packages

--- fedora/install/packages.sh
+++ fedora/install/packages.sh
@@ -2,20 +2,20 @@
 
 # List of packages to install (add or remove packages as needed)
 packages_to_install=(
-  gnome-tweaks
+  azure-cli
   code
-  htop
-  tree
-  vlc
   curl
-  fzf
-  zsh
   dotnet-sdk-6.0
   dotnet-sdk-7.0
   dotnet-sdk-8.0
+  fzf
+  gnome-tweaks
+  htop
   neovim
   python3-neovim
-  azure-cli
+  tree
+  vlc
+  zsh
 )
 
 # Install packages
