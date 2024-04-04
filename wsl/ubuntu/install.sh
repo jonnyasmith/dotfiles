f1a9be6f3771082c8084e63c5cc7eac7d1c1de10
Jonny Smith <jonny.smith@digitaljs.com>
2024-04-04 16:47:31 +0100

    Update WSL Ubuntu installation script to symlink dotfiles and install oh-my-zsh

--- wsl/ubuntu/install.sh
+++ wsl/ubuntu/install.sh
@@ -7,9 +7,9 @@ source install/packages.sh
 echo "Configure Linux..."
 source install/linux.sh
 
-# echo ""
-# echo "Symlinking dotfiles..."
-# source install/links.sh
+echo ""
+echo "Symlinking dotfiles..."
+source install/links.sh
 
 
 echo ""
