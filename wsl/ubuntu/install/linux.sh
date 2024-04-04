03811f2e98a6223b5f12c0f681c4349ff515a935
Jonny Smith <jonny.asmith@gmail.com>
2024-04-04 16:47:31 +0100

    Update WSL Ubuntu installation script to symlink dotfiles and install oh-my-zsh

--- wsl/ubuntu/install/linux.sh
+++ wsl/ubuntu/install/linux.sh
@@ -10,3 +10,9 @@ then
 else
     echo "Starship is already installed"
 fi
+
+if [ ! -d "$HOME/.oh-my-zsh" ] ; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    echo "oh-my-zsh is already installed"
+fi
