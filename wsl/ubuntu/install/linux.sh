769f351ad6d439e2e0635db63de7cf4c89531cc1
Jonny Smith <jonny.smith@digitaljs.com>
2024-04-04 16:37:31 +0100

    Add starship installation to linux.sh

--- wsl/ubuntu/install/linux.sh
+++ wsl/ubuntu/install/linux.sh
@@ -3,3 +3,10 @@
 if [ ! -d "/home/jonny/.local/share/nvim/site/pack/packer/start/packer.nvim" ] ; then
     git clone https://github.com/wbthomason/packer.nvim /home/jonny/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
+
+if ! command -v starship &> /dev/null
+then
+    curl -sS https://starship.rs/install.sh | sh
+else
+    echo "Starship is already installed"
+fi
