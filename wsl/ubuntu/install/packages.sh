6b117f30cbc204dbe750848a1a4629a1ca857301
Jonny Smith <jonny.asmith@gmail.com>
2024-04-04 16:33:34 +0100

    Update package installation script

--- wsl/ubuntu/install/packages.sh
+++ wsl/ubuntu/install/packages.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Check if apt is installed
+if ! which apt &> /dev/null
+then
+    printf "apt could not be found\n"
+    exit
+fi
+
+# Update the package list and upgrade installed packages
+echo "Updating packages..."
+sudo apt update
+sudo apt upgrade -y
+
 # List of packages to install (add or remove packages as needed)
 packages_to_install=(
   curl
