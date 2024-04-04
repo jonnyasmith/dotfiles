b8392360ecc91a7ed08230edea770886c27d1a92
Jonny Smith <jonny.smith@digitaljs.com>
2024-04-04 16:35:18 +0100

    Add packer.nvim installation to linux.sh script

--- wsl/ubuntu/install/linux.sh
+++ wsl/ubuntu/install/linux.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Check if apt is installed
-if ! which apt &> /dev/null
-then
-    printf "apt could not be found\n"
-    exit
+if [ ! -d "/home/jonny/.local/share/nvim/site/pack/packer/start/packer.nvim" ] ; then
+    git clone https://github.com/wbthomason/packer.nvim /home/jonny/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
-
-# Update the package list and upgrade installed packages
-echo "Updating packages..."
-sudo apt update
-sudo apt upgrade -y
-
-# if [ ! -d "/home/jonny/.local/share/nvim/site/pack/packer/start/packer.nvim" ] ; then
-#     git clone https://github.com/wbthomason/packer.nvim /home/jonny/.local/share/nvim/site/pack/packer/start/packer.nvim
-# fi
