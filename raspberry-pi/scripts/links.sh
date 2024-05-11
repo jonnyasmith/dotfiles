bbe6209659f1006475c1bf689749867861baa9ef
Jonny Smith <jonny.asmith@gmail.com>
2024-05-11 16:04:24 +0100

    Refactor Raspberry Pi installation script to symlink starship configuration

--- raspberry-pi/scripts/links.sh
+++ raspberry-pi/scripts/links.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Symlinking starship..."
+ln -sf /home/jonny/.dotfiles/raspberry-pi/dotfiles/starship.toml /home/jonny/.config/starship.toml
+
 echo "Symlinking gitconfig..."
 ln -sf /home/jonny/.dotfiles/raspberry-pi/dotfiles/.gitconfig /home/jonny/.gitconfig
 
