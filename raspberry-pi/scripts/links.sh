bcbeba1f1b0a695aa7f09904b05a6ab7863a1618
Jonny Smith <jonny.asmith@gmail.com>
2024-05-11 15:52:45 +0100

    Refactor Raspberry Pi installation script to fix gitconfig and zshrc symlinks

--- raspberry-pi/scripts/links.sh
+++ raspberry-pi/scripts/links.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Symlinking gitconfig..."
-ln -sf ~/.dotfiles/raspberry-pi/dotfiles/.gitconfig /home/jonny/.gitconfig
+ln -sf /home/jonny/.dotfiles/raspberry-pi/dotfiles/.gitconfig /home/jonny/.gitconfig
 
 echo "Symlinking zshrc..."
-ln -sf ~/.dotfiles/raspberry-pi/dotfiles/.zshrc /home/jonny/.zshrc
+ln -sf /home/jonny/.dotfiles/raspberry-pi/dotfiles/.zshrc /home/jonny/.zshrc
