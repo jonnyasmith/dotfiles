280e69dca927a4cc216dc4ef47bdc41bea01a31c
Jonny Smith <jonny.smith@dywidga.com>
2024-05-11 15:08:55 +0100

    chore: Comment out git clone command in linux.sh

--- raspberry-pi/scripts/linux.sh
+++ raspberry-pi/scripts/linux.sh
@@ -12,6 +12,6 @@ echo "Updating packages..."
 sudo apt update
 sudo apt upgrade -y
 
-if [ ! -d "/home/jonny/.local/share/nvim/site/pack/packer/start/packer.nvim" ] ; then
-    git clone https://github.com/wbthomason/packer.nvim /home/jonny/.local/share/nvim/site/pack/packer/start/packer.nvim
-fi
+# if [ ! -d "/home/jonny/.local/share/nvim/site/pack/packer/start/packer.nvim" ] ; then
+#     git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# fi
